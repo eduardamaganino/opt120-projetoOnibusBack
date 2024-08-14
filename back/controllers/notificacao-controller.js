c2d9696e88bd06a7a875de6b70b77235a86e6ce3
Eduarda Vaz Maganino <98330048+eduardamaganino@users.noreply.github.com>
2024-08-14 20:50:01 -0300

    crud cartao e notificacao

---
+++ back/controllers/notificacao-controller.js
@@ -0,0 +1,76 @@
+const database = require('../database/connection');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+class NotificacaoController {
+    create(req, res) {
+        const { idUser, texto, dataHora, isRead } = req.body;
+
+        database.query(
+            'INSERT INTO optbusao.notificacoes (idUser, texto, dataHora, isRead ) VALUES (?, ?, ?, ?)',
+            [idUser, texto, dataHora, isRead ],
+            (err, results) => {
+                if (err) {
+                    console.error(err);
+                    res.status(500).json({ error: 'Erro interno do servidor' });
+                    return;
+                }
+                console.log(results);
+                res.json(results);
+            }
+        );
+    }
+
+    getById(req, res) {
+        const { id } = req.params;
+        const query = 'SELECT * FROM optbusao.notificacoes WHERE id = ?';
+
+        database.query(query, [id], (error, results) => {
+            if (error) {
+                console.error(error);
+                res.status(500).json({ error: 'Erro interno do servidor' });
+                return;
+            }
+
+            if (results.length === 0) {
+                res.status(404).json({ error: 'Notificação não encontrado' });
+                return;
+            }
+
+            const usuario = results[0];
+            res.json(usuario);
+        });
+    }
+
+    delete(req, res) {
+        const { id } = req.params;
+
+        // Check if the user exists
+        database.query('SELECT * FROM optbusao.notificacoes WHERE id = ?', [id], (error, results) => {
+            if (error) {
+                console.error(error);
+                res.status(500).json({ error: 'Erro interno do servidor' });
+                return;
+            }
+
+            if (results.length === 0) {
+                res.status(404).json({ error: 'Notificação não encontrado' });
+                return;
+            }
+
+            // Delete the user
+            database.query('DELETE FROM optbusao.notificacoes WHERE id = ?', [id], (deleteError) => {
+                if (deleteError) {
+                    console.error(deleteError);
+                    res.status(500).json({ error: 'Erro ao deletar a notificação' });
+                    return;
+                }
+
+                res.json({ message: 'Notificação deletada com sucesso' });
+            });
+        });
+    }
+
+}
+
+module.exports = new NotificacaoController;
--- back/routes.js
+++ back/routes.js
@@ -2,6 +2,8 @@ const connection = require('./database/connection')
 const express = require('express')
 const router = express.Router()
 const UsuarioController = require('./controllers/usuario-controller')
+const NotificacaoController = require('./controllers/notificacao-controller')
+const CartaoController = require('./controllers/cartao-controller')
 
 router.post('/login', UsuarioController.login)
 router.post('/newUser',UsuarioController.newUser)
@@ -9,5 +11,14 @@ router.get('/showUser', UsuarioController.showUser)
 router.get('/showUserId/:id', UsuarioController.showUserById)
 router.delete('/deleteUser/:id', UsuarioController.deleteUser)
 router.put('/updateUser/:id', UsuarioController.updateUser)
+
+router.post('/create', NotificacaoController.create)
+router.get('/getById/:id', NotificacaoController.getById)
+router.delete('/delete/:id', NotificacaoController.delete)
+
+router.post('/create', CartaoController.create)
+router.get('/getById/:id', CartaoController.getById)
+router.get('/getByIdUser/:id', CartaoController.getByIdUser)
+router.delete('/delete/:id', CartaoController.delete)
 
 module.exports = router

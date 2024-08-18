098c43c4e84a7849b107908560ff91801d9f1b6b
Elder <elderh.correia@gmailcom>
2024-08-18 02:31:20 -0300

    Adicionado Get e Post referente as notificações

---
+++ opt120-projetoOnibusBack/back/routes.js
@@ -0,0 +1,27 @@
+const connection = require('./database/connection')
+const express = require('express')
+const router = express.Router()
+const UsuarioController = require('./controllers/usuario-controller')
+const NotificacaoController = require('./controllers/notificacao-controller')
+const CartaoController = require('./controllers/cartao-controller')
+
+router.post('/login', UsuarioController.login)
+router.post('/newUser',UsuarioController.newUser)
+router.get('/showUser', UsuarioController.showUser)
+router.get('/showUserId/:id', UsuarioController.showUserById)
+router.delete('/deleteUser/:id', UsuarioController.deleteUser)
+router.put('/updateUser/:id', UsuarioController.updateUser)
+
+router.post('/createNotificacao', NotificacaoController.create)
+router.get('/getByIdNotificacao/:id', NotificacaoController.getById)
+router.get('/getNotificationsByUserId/:idUser', NotificacaoController.getByUserId);
+router.delete('/deleteNotificacao/:id', NotificacaoController.delete)
+// Rota para atualizar o status da notificação
+router.post('/updateNotificationStatus/:id', NotificacaoController.updateStatus);
+
+router.post('/createCartao', CartaoController.create)
+router.get('/getByIdCartao/:id', CartaoController.getById)
+router.get('/getByIdUserCartao/:id', CartaoController.getByIdUser)
+router.delete('/deleteCartao/:id', CartaoController.delete)
+
+module.exports = router
